f601d5f4be40b92445692c8655c950501fb98ce1
unknown <jerikosalivan@gmail.com>
2020-04-25 17:55:26 +0900

    added database

--- main.dart
+++ main.dart
@@ -1,12 +1,10 @@
 import 'results.dart';
 
 void main() {
-  var temp1 = placeCounter(random_24_04);
-  var temp2 = placeCounter(random_23_04);
-  var merged = mergeMaps(temp1, temp2);
-  // print(temp1);
-  // print(temp2);
-  // print(merged);
+  Map<String, List<int>> temp1 = placeCounter(random_24_04);
+  Map<String, List<int>> temp2 = placeCounter(random_23_04);
+  Map<String, List<int>> merged = mergeMaps(temp1, temp2);
+
   for (String key in merged.keys) {
     print("$key :" + " ${merged[key]}");
   }
@@ -16,14 +14,9 @@ Map<String, List<int>> placeCounter(Map<int, List<String>> values) {
   Map<String, List<int>> placement = {};
   for (int i = 1; i < values.length; i++) {
     for (int j = 0; j < 4; j++) {
-      if (placement.keys != values[i][j]) {
-        List<int> place = List.filled(10, 0);
-        place[i - 1] = 1;
-        placement.addAll({values[i][j]: place});
-      }
-      // else {
-      //   ++placement[values[i][j]][i];
-      // }
+      List<int> place = List.filled(10, 0);
+      place[i - 1] = 1;
+      placement.addAll({values[i][j]: place});
     }
   }
   return placement;
--- results.dart
+++ results.dart
@@ -15,3 +15,89 @@ Map<int, List<String>> random_23_04 = {
   7: ["michjke", "diq2s", "marshmellow", "second", "hoshi"],
   8: ["qinee", "dejavu", "cara_delevin", "heso", "wyd"]
 };
+
+Map<int, List<String>> random_22_04 = {
+  1: ["kenchery", "rvze", "scooby", "efef", "wyd"],
+  2: ["second", "watcher", "hikari", "paradisescream", "hellscream"],
+  3: ["vld", "duke", "blazebite", "marshmellow", "missalena"],
+  4: ["foqd", "solork", "buuren", "sanya", "links"],
+  5: ["hoshi", "kuraj", "sora", "keen", "heso"],
+  6: ["shumer", "diq2s", "hack", "sudden", "michilnoob"],
+  7: ["9k", "shaddo", "jeriko", "hellmuth", "chloe"],
+  8: ["sth", "warasar", "deadstar", "knohlead", "yaky"]
+};
+
+Map<int, List<String>> random_19_04 = {
+  1: ["hack", "sudden", "scooby", "subary", "eavest"],
+  2: ["kuraj", "hikari", "keen", "vld", "vasse"],
+  3: ["fullmetal", "brz", "knohlead", "r1", "kenchery"],
+  4: ["sora", "hallelujah", "chloe", "slazer", "9k"],
+  5: ["jeriko", "19_17_17_111", "pfgwl", "second", "ubi"],
+  6: ["diq2s", "sanya", "hellmuth", "foqd", "zxc123qwe"],
+  7: ["diq2s", "plex", "hellmuth", "michilnoob", "trap_luv"]
+};
+
+Map<int, List<String>> random_18_04 = {
+  1: ["efef", "kuraj", "jeriko", "hellscream", "vld"],
+  2: ["duke", "plex", "scooby", "buuren", "kenchery"],
+  3: ["vovantus", "solork", "rave", "hellmuth", "michilnoob"],
+  4: ["lucas", "diq2s", "dred", "foqd", "second"],
+  5: ["dejavu", "hikari", "paradisescream", "keen", "molodoy"],
+  6: ["sora", "vasya", "marshmellow", "togu", "hoshi"],
+  7: ["fullmetal", "diq2s", "asd", "togu", "eavest"],
+  8: ["madjahet", "blazebite", "dred", "deadstar", "9k"],
+  9: ["lukas", "vasya", "19_17_17_111", "knohlead", "second"],
+  10: ["brz", "warasar", "marshmellow", "foqd", "hoshi"]
+};
+
+Map<int, List<String>> random_15_04 = {
+  1: ["vovantus", "diq2s", "paradisescream", "foqd", "heso"],
+  2: ["lefitan", "plex", "hallelujah", "lacalut", "kenchery"],
+  3: ["lucas", "vasyasplash", "jeriko", "trap_luv", "luna"],
+  4: ["sora", "solork", "second", "wyd", "nulevoy"],
+  5: ["watcher", "kuraj", "knohlead", "just", "jerve"],
+  6: ["shaddo", "rave", "efef", "9k", "negrofan"]
+};
+
+Map<int, List<String>> random_14_04 = {
+  1: ["fullmetal", "brz", "plex", "togu", "subaru"],
+  2: ["vova3b", "diq2s", "cara_delevin", "foqd", "madness"],
+  3: ["legolas", "vasya", "rave", "deadstar", "molodoy"],
+  4: ["lukas", "shaddo", "jeriko", "knohlead", "sth"],
+  5: ["tashu", "kuraj", "hikari", "bdk", "kenchery"],
+  6: ["solork", "blure", "efef", "9k", "second"]
+};
+
+Map<int, List<String>> random_13_04 = {
+  1: ["brz", "plex", "dred", "paradisescream", "subaru"],
+  2: ["duke", "shaddo", "buuren", "efef", "foqd"],
+  3: ["solork", "vasya", "second", "durachyo", "kenchery"],
+  4: ["watcher", "blure", "bitw13", "heso", "bdk"],
+  5: ["fullmetal", "jeriko", "hellmuth", "mirsser", "dh"]
+};
+
+Map<int, List<String>> random_12_04 = {
+  1: ["dejavu", "hikari", "efef", "keen", "19_17_17_111"],
+  2: ["madjaget", "kuraj", "diq2s", "makaron", "ubi"],
+  3: ["sora", "rave", "togich", "second", "kenchery"],
+  4: ["12", "dred", "vasya", "deadstar", "meller"],
+  5: ["marshmellow", "solork", "plex", "warasar", "heso"],
+  6: ["shaddo", "halleluja", "bdk", "jeriko", "hellmuth"]
+};
+
+Map<int, List<String>> random_11_04 = {
+  1: ["sora", "solork", "vasya", "second", "nuwopognale"],
+  2: ["brz", "hikari", "yorik", "bdk", "sever"],
+  3: ["scooby", "diq2s", "kenchery", "19_17_17_111", "keen"],
+  4: ["fullmetal", "plex", "blazebite", "foqd", "kabal"],
+  5: ["jeriko", "kuraj", "wyd", "retpol", "32_i_devki_tekut"],
+  6: ["shaddo", "marshmallow", "mumillenium", "heso", "yakutza"]
+};
+
+Map<int, List<String>> random_10_04 = {
+  1: ["diq2s", "dred", "kenchery", "stalky", "rave"],
+  2: ["scooby", "hikari", "plex", "ubi", "retpol"],
+  3: ["bdk", "marshmellow", "heso", "molodoy", "paradisescream"],
+  4: ["efef", "blazebite", "deadstar", "sth", "stair"],
+  5: ["dejavu", "vasya", "delusion", "hellmuth"]
+};
